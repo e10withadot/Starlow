4477a8eef619ceaa94092c805515d5f9efc58ab6
e10withadot <e10withadot@gmail.com>
2025-05-07 15:39:00 +0300

    Fixed error on termination

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import config as c
 from commands.bot_instance import StarlowBot
 import hikari
 
 bot = StarlowBot("[YOUR TOKEN HERE]")
+
+
 @bot.listen()
 async def load_commands(event: hikari.StartedEvent) -> None:
-	app= await bot.rest.fetch_application()
-	await bot.rest.set_application_commands(
-		app.id,
-		bot.build_commands()
-	)
+    app = await bot.rest.fetch_application()
+    await bot.rest.set_application_commands(
+        app.id,
+        bot.build_commands()
+    )
 
-# Run the bot
-bot.run(asyncio_debug=True, coroutine_tracking_depth=20, propagate_interrupts=True)
+try:
+    bot.run()
+except hikari.HikariInterrupt:
+    pass
