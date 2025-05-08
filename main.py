9d23fbac56541326ba3df620d83be681109db09f
e10withadot <e10withadot@gmail.com>
2025-05-08 19:38:06 +0300

    added "Playing.." activity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from commands.bot_instance import StarlowBot
+from random import choice
 import hikari
 
 bot = StarlowBot("[YOUR TOKEN HERE]")
@@ -13,6 +14,25 @@ async def load_commands(event: hikari.StartedEvent) -> None:
     )
 
 try:
-    bot.run()
+    games = ["Super Mario RPG: Legend of the Seven Stars",
+             "Paper Mario",
+             "Mario & Luigi: Superstar Saga",
+             "Paper Mario: The Thousand-Year Door",
+             "Mario & Luigi: Partners in Time",
+             "Super Paper Mario",
+             "Mario & Luigi: Bowser's Inside Story",
+             "Paper Mario: Sticker Star",
+             "Mario & Luigi: Dream Team",
+             "Mario & Luigi: Paper Jam",
+             "Paper Mario: Color Splash",
+             "Minion Quest: The Search for Bowser",
+             "Mario + Rabbids: Kingdom Battle",
+             "Bowser Jr.'s Journey",
+             "Paper Mario: The Origami King",
+             "Mario + Rabbids: Sparks of Hope",
+             "Mario & Luigi: Brothership"
+             ]
+    activity = hikari.Activity(name=choice(games))
+    bot.run(activity=activity)
 except hikari.HikariInterrupt:
     pass
